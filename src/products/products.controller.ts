71a954636dae041bfe5f8a8bacfb97c2531c0760
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-08 16:26:21 +0100

    add: endpoint Get- getAll

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,1 +1,16 @@
-export class ProductsController {}
+import { ProductsService } from './products.service';
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  private productsService;
+
+  constructor(productService: ProductsService) {
+    this.productsService = productService;
+  }
+
+  @Get('/')
+  getAll(): any {
+    return this.productsService.getAll();
+  }
+}
