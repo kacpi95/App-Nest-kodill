fc8a6e8ffa27ab760675ea80cb7ca473d947608d
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-13 21:58:08 +0100

    update: method create orders

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,5 +1,5 @@
 import { Order } from '@prisma/client';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/services/prisma.service';
 
 @Injectable()
@@ -21,18 +21,24 @@ export class OrdersService {
       where: { id },
     });
   }
-  public create(
+  public async create(
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
     const { productId, ...otherData } = orderData;
-    return this.prismaService.order.create({
-      data: {
-        ...otherData,
-        product: {
-          connect: { id: productId },
+    try {
+      return await this.prismaService.order.create({
+        data: {
+          ...otherData,
+          product: {
+            connect: { id: productId },
+          },
         },
-      },
-    });
+      });
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new BadRequestException("Product doesn't exist");
+      throw error;
+    }
   }
   public updateById(
     id: Order['id'],
