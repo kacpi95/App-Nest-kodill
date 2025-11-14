c9f9dcd0c3f365237ea275fff9c966c781428cab
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-14 21:50:32 +0100

    update: dtos create-orders

--- src/products/dtos/create-orders.dtos.ts
+++ src/products/dtos/create-orders.dtos.ts
@@ -1,20 +1,11 @@
-// import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsString, IsUUID, Length } from 'class-validator';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class CreateOrderDTO {
   @IsNotEmpty()
-  @IsString()
   @IsUUID()
   productId: string;
 
   @IsNotEmpty()
-  @IsString()
-  @Length(5, 20)
-  client: string;
-
-  // @IsNotEmpty()
-  // @IsString()
-  // @Length(5, 100)
-  // @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
-  address: string;
+  @IsUUID()
+  clientId: string;
 }
