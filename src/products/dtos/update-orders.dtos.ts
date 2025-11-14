51965bc0899d3c133a7c87f263885c9f9ac326fd
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-14 22:14:19 +0100

    update: dtos update-orders

--- src/products/dtos/update-orders.dtos.ts
+++ src/products/dtos/update-orders.dtos.ts
@@ -1,21 +1,11 @@
-import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsString, IsUUID, Length } from 'class-validator';
+import { IsUUID, IsOptional } from 'class-validator';
 
 export class UpdateOrderDTO {
-  @IsNotEmpty()
-  @IsString()
-  @Length(5, 20)
+  @IsOptional()
   @IsUUID()
-  productId: string;
+  productId?: string;
 
-  @IsNotEmpty()
-  @IsString()
-  @Length(5, 20)
-  client: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @Length(5, 100)
-  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : value))
-  address: string;
+  @IsOptional()
+  @IsUUID()
+  clientId?: string;
 }
